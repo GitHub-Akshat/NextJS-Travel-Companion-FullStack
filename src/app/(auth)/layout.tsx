134ff3c3d3b36de63d75a151c211848cb1a04fc7
Github-Akshat <akshatji21052003@gmail.com>
2024-11-01 15:23:48 +0530

    postgresql setup using prisma

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,6 +1,7 @@
 const AuthLayout = ({ children } : { children : React.ReactNode}) => {
   return (
-    <section className="w-full bg-neutral-100">
+    <section className='w-full relative'>
+        <div className="absolute inset-0 -z-10 h-full w-full bg-neutral-200 bg-[linear-gradient(to_right,#d0d0d0_1px,transparent_1px),linear-gradient(to_bottom,#d0d0d0_1px,transparent_1px)] bg-[size:6rem_4rem]"></div>
         <div className="h-screen flex items-center justify-center">
             { children }
         </div>
