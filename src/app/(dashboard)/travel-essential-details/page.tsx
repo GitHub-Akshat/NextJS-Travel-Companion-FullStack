c7acc1982c832dc03043e768090920417f356f27
Github-Akshat <akshatji21052003@gmail.com>
2024-11-29 12:31:40 +0530

    deployment ready

--- src/app/(dashboard)/travel-essential-details/page.tsx
+++ src/app/(dashboard)/travel-essential-details/page.tsx
@@ -1,8 +1,11 @@
 import Travel from "@/components/dashboard/travel"
+import { Suspense } from "react"
 
 const TravelEssentialDetails = () => {
   return (
-    <div><Travel/></div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div><Travel/></div>
+    </Suspense>
   )
 }
 export default TravelEssentialDetails
